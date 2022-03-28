9f8efcbefd5b1cf7ff17396f6bf40b5d3718013a
Nagulapally-Anudeep <nanudeep408@gmail.com>
2022-03-28 11:11:14 +0530

    added post feature

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,6 +1,27 @@
 const Post = require("../models/postModel");
+const User = require("../models/userModel");
 
-exports.createPost = async (req, res, next) => {};
+exports.createPost = async (req, res, next) => {
+  const createdUser = req.user;
+  const newPost = {
+    picture: req.body.picture,
+    caption: req.body.caption,
+    createdBy: createdUser._id,
+  };
+  const post = await Post.create(newPost);
+
+  const userPosts = createdUser.posts;
+  userPosts.push(post._id);
+
+  await User.findByIdAndUpdate(
+    createdUser._id,
+    { $set: { posts: userPosts } },
+    { new: true }
+  );
+
+  res.redirect("/");
+};
+
 exports.likePost = async (req, res, next) => {};
 exports.unlikePost = async (req, res, next) => {};
 exports.comment = async (req, res, next) => {};
--- models/postModel.js
+++ models/postModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-  pic: {
+  picture: {
     type: String,
   },
   caption: {

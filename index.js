74bb020f06387ce18e971fbc55258d40ed0eecc6
Sathiya Seelan <codewithsathya@gmail.com>
2022-03-28 11:10:49 +0530

    initialized google oauth

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const session = require("cookie-session");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const app = express();
 
-//setup ejs
-app.set('view engine', 'ejs');
+app.use(cors());
 
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+
+		}
+  )
+);
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({ secret: "shinzou sasageyo", key: "levi"}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Server is running on port " + port));
